4f3b7a67f6fdf18ad4fe41a7cbed095840b832bc
Ariel Manzur <ariel@godotengine.org>
2025-09-25 23:23:40 -0300

    adding item

---
+++ demo/item.gd
@@ -0,0 +1,17 @@
+extends Node3D
+
+var type
+var index
+var area
+var epoc
+
+
+func set_item_info(p_index, p_type, p_area, p_epoc):
+	type = p_type
+	index = p_index
+	area = p_area
+	epoc = p_epoc
+	
+
+func _read():
+	pass
